04b1a85c828aa275c50156ce187709f0524b198a
Remus Selea <remus.selea@gmail.com>
2023-10-29 20:12:41 +0200

    Style the navbar

--- frontend/src/components/Navbar/NavBar.tsx
+++ frontend/src/components/Navbar/NavBar.tsx
@@ -1,17 +1,39 @@
-import { NavLink } from 'react-router-dom';
-import './navbar.css';
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+
 const Navbar = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Dashboard</NavLink>
+      <ul className="nav-list">
+        <li className="nav-list-item">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `nav-link ${isActive ? "nav-link-active" : ""}`
+            }
+          >
+            Dashboard
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="/profile">Profile</NavLink>
+        <li className="nav-list-item">
+          <NavLink
+            to="/profile"
+            className={({ isActive }) =>
+              `nav-link ${isActive ? "nav-link-active" : ""}`
+            }
+          >
+            Profile
+          </NavLink>
         </li>
-        <li>
-          <NavLink to="/statements">Statements</NavLink>
+        <li className="nav-list-item">
+          <NavLink
+            to="/statements"
+            className={({ isActive }) =>
+              `nav-link ${isActive ? "nav-link-active" : ""}`
+            }
+          >
+            Statements
+          </NavLink>
         </li>
       </ul>
     </nav>
