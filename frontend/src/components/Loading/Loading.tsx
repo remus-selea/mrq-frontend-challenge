f6463eb946a5bd16e81cbdef557671368343e315
Remus Selea <remus.selea@gmail.com>
2023-11-04 22:46:19 +0200

    Cache the Loading component with memo

--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -1,11 +1,12 @@
+import { memo } from "react";
 import "./loading.css";
 
-const Loading = () => {
+const Loading = memo(function Loading() {
   return (
     <div className="loading">
       <div className="loading__pulse" />
     </div>
   );
-};
+});
 
 export default Loading;
